2d15a429dabb477391093084bae5d34c0414c548
samienda <134318401+samienda@users.noreply.github.com>
2023-08-03 23:11:11 +0300

    Time: 54 ms (57.88%), Space: 16.2 MB (64.9%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,41 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 1 or x == 2 or x == 3:
+            return 1
+        low = 0
+        high = x
+        mid = x // 2
+
+        while low < high:
+            mid = (high + low) // 2
+            if (mid ** 2) > x:
+                if ((mid - 1)**2) < x:
+                    return mid -1 
+                high = mid
+            elif mid** 2 < x:
+                if (mid + 1) ** 2 > x:
+                    return mid
+                low = mid
+            else:
+                return mid
+
+
+        return mid
+
+        high = x
+        low = 0
+        mid = x // 2
+        while low < high:
+            mid = (low + high) // 2
+            if (mid ** 2)  > x:
+                if ((mid - 1) ** 2) < x:
+                    return mid - 1
+                high = mid 
+            elif (mid ** 2) < x:
+                if ((mid + 1) ** 2) > x:
+                    return mid
+                low = mid 
+            else: 
+                return mid
+
+        return mid
