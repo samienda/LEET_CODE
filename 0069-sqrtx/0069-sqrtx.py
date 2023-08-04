b9a3524af00822bb96ea1892223e44184946d325
samienda <134318401+samienda@users.noreply.github.com>
2023-08-04 20:53:59 +0300

    Update 0069-sqrtx.py

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -21,21 +21,3 @@ class Solution:
 
 
         return mid
-
-        high = x
-        low = 0
-        mid = x // 2
-        while low < high:
-            mid = (low + high) // 2
-            if (mid ** 2)  > x:
-                if ((mid - 1) ** 2) < x:
-                    return mid - 1
-                high = mid 
-            elif (mid ** 2) < x:
-                if ((mid + 1) ** 2) > x:
-                    return mid
-                low = mid 
-            else: 
-                return mid
-
-        return mid
