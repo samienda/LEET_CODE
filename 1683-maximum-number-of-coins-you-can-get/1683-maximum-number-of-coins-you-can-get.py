5c61587d24f21db83a91dc47f90ca870e210f0c9
samienda <134318401+samienda@users.noreply.github.com>
2023-10-04 18:35:46 +0300

    Time: 542 ms (25.37%), Space: 28.8 MB (36.1%) - LeetHub

--- 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.py
+++ 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.py
@@ -1,41 +1,39 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
-        # n = len(piles) // 3
-        # alice = 0
         # me = 0
-        # bob = 0
+        # maxi = max(piles)
 
-        # for i in range(n):
-        #     ali = max(piles)
-        #     piles.remove(ali)
-        #     m = max(piles)
-        #     piles.remove(m)
+        # result = [0 for i in range(maxi + 1)]
+        # for i in piles:
+        #     result[i] += 1
+
+        # last = []
+
+        # for i in range(maxi + 1):
+        #     for j in range(result[i]):
+        #         last.append(i)
+
+        # for i in range(len(piles)//3):
+        #     last.pop()
+        #     m = last.pop()
         #     me += m
 
         # return me
 
 
-        me = 0
-        maxi = max(piles)
+        piles.sort()
+        result = 0
+        left = 0
+        right = len(piles) - 2
 
-        result = [0 for i in range(maxi + 1)]
-        for i in piles:
-            result[i] += 1
-
-        last = []
-
-        for i in range(maxi + 1):
-            for j in range(result[i]):
-                last.append(i)
-
-        for i in range(len(piles)//3):
-            last.pop()
-            m = last.pop()
-            me += m
+        while left < right:
+            result += piles[right]
 
 
-        print(last)
-        return me
+            left += 1
+            right -= 2
+
+        return result
 
 
 
