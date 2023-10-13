61a7f28045dcdbe14440276896c54e0fabfd2a97
samienda <134318401+samienda@users.noreply.github.com>
2023-10-13 19:13:26 +0300

    Time: 956 ms (60.64%), Space: 30.1 MB (97.53%) - LeetHub

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        n = len(nums)
+        l = 0
+
+        windowsum = 0
+        oper = -1
+
+        value = sum(nums) - x
+
+
+        for r in range(n):
+            windowsum += nums[r]
+
+            while windowsum > value and l <= r:
+                windowsum -= nums[l]
+                l += 1
+
+            if windowsum == value:
+                oper = max(oper, r - l + 1)
+
+
+        return -1 if oper == -1 else n - oper
+
+
+        
